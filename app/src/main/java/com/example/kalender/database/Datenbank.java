955921302e8e0242289baa4ceb29c5d2c602a2a0
marlene <marlene78@schule.bremen.de>
2022-03-22 22:28:12 +0100

    SQLite datenbank 2

--- app/src/main/java/com/example/kalender/database/Datenbank.java
+++ app/src/main/java/com/example/kalender/database/Datenbank.java
@@ -134,12 +134,6 @@ public class Datenbank {
         }
     }
 
-    //Test
-    public String test()
-    {
-        return "Test";
-    }
-
 
     public void datensatzEinfuegen(String pDatum, String pAktivitaet, String pUhrzeit)
     {
